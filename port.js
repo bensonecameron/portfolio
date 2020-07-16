6a9f4e43010c24b8830b2689591266b99eef623b
Cameron Benson <66973535+bensonecameron@users.noreply.github.com>
2020-07-16 00:07:22 -0400

    First commit

---
+++ port.js
@@ -0,0 +1,12 @@
+console.log("There's a game afoot...")
+
+function boom() {
+    location.replace("img/boom.gif")
+  }
+
+// <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/g2YdApKEna2sg" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/gifs/bomb-g2YdApKEna2sg">via GIPHY</a></p>
+
+// I'll have to add an event listener on click 
+
+// typing carosel https://speckyboy.com/css-javascript-text-animation-snippets/
+
